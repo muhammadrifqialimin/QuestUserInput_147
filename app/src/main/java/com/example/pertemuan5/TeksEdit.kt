110f4defb8422290dc317ce44d7a6e0f0d728013
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-30 16:17:56 +0700

    terapkan ketebalan dan warna divider kustom

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -104,3 +104,6 @@ fun FormDataDiri(modifier: Modifier
                     top = dimensionResource(
                         id = R.dimen.padding_medium
                     )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )

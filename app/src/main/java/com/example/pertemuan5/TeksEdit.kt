e1aef4a52ab7f1008b5e4f0f488e33696946b774
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-30 15:04:04 +0700

    tambah resource dimensi untuk padding dan divider

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,9 +1,15 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember {mut}
 }

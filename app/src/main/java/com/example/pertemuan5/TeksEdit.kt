4b36708d98c8d8054f31f4e798c3d0675ad2fb3d
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-30 16:17:14 +0700

    Menambahkan HorizontalDivider dengan padding atas & bawah menggunakan resource dimensi untuk memperjelas pemisahan section form.

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -98,3 +98,9 @@ fun FormDataDiri(modifier: Modifier
             Text(text = stringResource(id = R.string.submit))
         }
 
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(id = R.dimen.padding_medium),
+                    top = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )),
